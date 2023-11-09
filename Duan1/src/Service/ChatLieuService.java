5c8e98a3a40b23f4ceb096e7c0de3f3dce804727
ISundev <sunnnne04@gmail.com>
2023-11-09 18:08:35 +0700

    aaa

--- Duan1/src/Service/ChatLieuService.java
+++ Duan1/src/Service/ChatLieuService.java
@@ -3,11 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Service;
-
+import Model.ChatLieu;
+import java.sql.*;
+import java.util.List;
 /**
  *
  * @author admin
  */
 public class ChatLieuService {
-    
+    List<ChatLieu> getAll(){
+        
+    }
 }

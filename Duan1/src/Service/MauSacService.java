386d1e6e07de939e2d618f7b7cddd317514a0c2e
bhlong1710 <builong1710@gmail.com>
2023-11-09 18:08:54 +0700

    333

--- Duan1/src/Service/MauSacService.java
+++ Duan1/src/Service/MauSacService.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Service;
 
-/**
- *
- * @author admin
- */
+
 public class MauSacService {
-    
+    public static void main(String[] args) {
+        System.out.println("22222");
+        System.out.println("22222");
+        System.out.println("22222");
+        System.out.println("22222");
+        
+    }
 }

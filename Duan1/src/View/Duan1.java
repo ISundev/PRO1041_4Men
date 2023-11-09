1f3cbcc369d400ff4afd9f683d540f647fb6a22a
bhlong1710 <builong1710@gmail.com>
2023-11-09 17:38:44 +0700

    111

--- Duan1/src/View/Duan1.java
+++ Duan1/src/View/Duan1.java
@@ -14,7 +14,10 @@ public class Duan1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("123123");
+        System.out.println("123123");
+        System.out.println("123123");
+        System.out.println("123123");
     }
     
 }
